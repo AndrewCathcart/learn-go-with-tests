e933c614fd64adda449d5f91a61c999969ea2687
Andrew Cathcart <newcastleandrewc@gmail.com>
2020-11-08 19:59:04 +0000

    withdraw and refactoring tests

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -5,13 +5,25 @@ import (
 )
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
-	wallet.Deposit(10)
 
-	actual := wallet.Balance()
-	expected := Bitcoin(10)
+	assertbalance := func(t *testing.T, wallet Wallet, expected Bitcoin) {
+		t.Helper()
+		actual := wallet.Balance()
 
-	if actual != expected {
-		t.Errorf("got %s expected %s", actual, expected)
+		if actual != expected {
+			t.Errorf("got %s expected %s", actual, expected)
+		}
 	}
+
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(10)
+		assertbalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		wallet.Withdraw(Bitcoin(10))
+		assertbalance(t, wallet, Bitcoin(10))
+	})
 }
