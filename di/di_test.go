d730a9b97424ed87f93122476f07ffd37f0092d0
Andrew Cathcart <newcastleandrewc@gmail.com>
2020-11-08 23:33:03 +0000

    some basic dependency injection with io.Writer

---
+++ di/di_test.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestGreet(t *testing.T) {
+	buffer := bytes.Buffer{}
+	Greet(&buffer, "Andy")
+
+	actual := buffer.String()
+	expected := "Hello, Andy"
+
+	if expected != actual {
+		t.Errorf("got %q expected %q", actual, expected)
+	}
+}
