09c353c2cf70e3b43694277d4a719543b878f1f9
Andrew Cathcart <newcastleandrewc@gmail.com>
2020-11-08 16:41:09 +0000

    hello with a name

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,8 +3,8 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	actual := Hello()
-	expected := "Hello, world!"
+	actual := Hello("Andy")
+	expected := "Hello, Andy!"
 
 	if actual != expected {
 		t.Errorf("got %q want %q", actual, expected)
