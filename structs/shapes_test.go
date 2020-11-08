e32e4f7a691feaf2814c224f138ca260a4aba14e
Andrew Cathcart <newcastleandrewc@gmail.com>
2020-11-08 19:09:13 +0000

    Can add names to tests

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -15,25 +15,28 @@ func TestPerimeter(t *testing.T) {
 	for _, tt := range perimeterTests {
 		actual := tt.shape.Perimeter()
 		if actual != tt.expected {
-			t.Errorf("got %g expected %g", actual, tt.expected)
+			t.Errorf("%#v got %g expected %g", tt.shape, actual, tt.expected)
 		}
 	}
 }
 
 func TestArea(t *testing.T) {
 	areaTests := []struct {
+		name     string
 		shape    Shape
 		expected float64
 	}{
-		{Rectangle{12, 6}, 72.0},
-		{Circle{10}, 314.1592653589793},
-		{Triangle{3, 5, 4}, 6},
+		{name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, expected: 72.0},
+		{name: "Circle", shape: Circle{Radius: 10}, expected: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{A: 3, B: 5, C: 4}, expected: 6},
 	}
 
 	for _, tt := range areaTests {
-		actual := tt.shape.Area()
-		if actual != tt.expected {
-			t.Errorf("got %g expected %g", actual, tt.expected)
-		}
+		t.Run(tt.name, func(t *testing.T) {
+			actual := tt.shape.Area()
+			if actual != tt.expected {
+				t.Errorf("%#v got %g expected %g", tt.shape, actual, tt.expected)
+			}
+		})
 	}
 }
