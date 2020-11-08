f877b4c39a191ce58930de036aac6c63fbe34d3f
Andrew Cathcart <newcastleandrewc@gmail.com>
2020-11-08 18:13:43 +0000

    arrays, slices, len, reflect.DeepEqual, test cov

--- arrays/sum.go
+++ arrays/sum.go
@@ -8,3 +8,27 @@ func Sum(numbers []int) int {
 	}
 	return sum
 }
+
+// SliceTotals takes a variable number of slices, and returns a new slice containing the totals for each provided slice
+func SliceTotals(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return sums
+}
+
+// SliceTailTotals takes a variable number of slices, and returns a new slice containing the tail totals (everything except the first value) for each provided slice
+func SliceTailTotals(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			sums = append(sums, Sum(numbers[1:]))
+		}
+	}
+
+	return sums
+}
--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -1,6 +1,9 @@
 package sum
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	numbers := []int{1, 2, 3}
@@ -11,4 +14,34 @@ func TestSum(t *testing.T) {
 	if actual != expected {
 		t.Errorf("actual %d expected %d given, %v", actual, expected, numbers)
 	}
+}
+
+func TestSliceTotals(t *testing.T) {
+	actual := SliceTotals([]int{1, 2}, []int{3, 4})
+	expected := []int{3, 7}
+
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("got %v want %v", actual, expected)
+	}
+}
+
+func TestSliceTailTotals(t *testing.T) {
+	checkSums := func(t *testing.T, actual, expected []int) {
+		t.Helper()
+		if !reflect.DeepEqual(actual, expected) {
+			t.Errorf("got %v want %v", actual, expected)
+		}
+	}
+
+	t.Run("sum tails of slices", func(t *testing.T) {
+		actual := SliceTailTotals([]int{1, 2}, []int{3, 4})
+		expected := []int{2, 4}
+		checkSums(t, actual, expected)
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		actual := SliceTailTotals([]int{}, []int{3, 4, 5})
+		expected := []int{0, 9}
+		checkSums(t, actual, expected)
+	})
 }
