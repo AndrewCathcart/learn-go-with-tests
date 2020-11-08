c42867883dda6297ad73a861d37788c0d558a98b
Andrew Cathcart <newcastleandrewc@gmail.com>
2020-11-08 20:14:39 +0000

    testing for errors with withdraw

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -5,16 +5,6 @@ import (
 )
 
 func TestWallet(t *testing.T) {
-
-	assertbalance := func(t *testing.T, wallet Wallet, expected Bitcoin) {
-		t.Helper()
-		actual := wallet.Balance()
-
-		if actual != expected {
-			t.Errorf("got %s expected %s", actual, expected)
-		}
-	}
-
 	t.Run("Deposit", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(10)
@@ -26,4 +16,32 @@ func TestWallet(t *testing.T) {
 		wallet.Withdraw(Bitcoin(10))
 		assertbalance(t, wallet, Bitcoin(10))
 	})
+
+	t.Run("Withdraw insufficient funds", func(t *testing.T) {
+		wallet := Wallet{Bitcoin(20)}
+		err := wallet.Withdraw(Bitcoin(100))
+
+		assertbalance(t, wallet, Bitcoin(20))
+		assertError(t, err, ErrInsufficientFunds)
+	})
+}
+
+func assertbalance(t *testing.T, wallet Wallet, expected Bitcoin) {
+	t.Helper()
+	actual := wallet.Balance()
+
+	if actual != expected {
+		t.Errorf("got %s expected %s", actual, expected)
+	}
+}
+
+func assertError(t *testing.T, actual error, expected error) {
+	t.Helper()
+	if actual == nil {
+		t.Fatal("wanted an error but didn't get one")
+	}
+
+	if actual != expected {
+		t.Errorf("got %q, expected %q", actual, expected)
+	}
 }
