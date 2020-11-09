683fcff26bc4dc33918aef4045840a354c2c1265
Andrew Cathcart <newcastleandrewc@gmail.com>
2020-11-09 00:50:56 +0000

    basic mocking for time.Sleep

---
+++ mocking/countdown_test.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestCountdown(t *testing.T) {
+	buffer := &bytes.Buffer{}
+	spySleeper := &SpySleeper{}
+	Countdown(buffer, spySleeper)
+
+	actual := buffer.String()
+	expected := `3
+2
+1
+Go!`
+
+	if actual != expected {
+		t.Errorf("got %q expected %q", actual, expected)
+	}
+
+	if spySleeper.Calls != 3 {
+		t.Errorf("not enough calls to sleeper, want 3 got %d", spySleeper.Calls)
+	}
+}
+
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
