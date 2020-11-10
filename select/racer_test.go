d9e4b3243cc38f5ca7f6597004773518c36511fb
Andrew Cathcart <newcastleandrewc@gmail.com>
2020-11-10 21:03:22 +0000

    racing two urls

---
+++ select/racer_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func TestRacer(t *testing.T) {
+	slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(20 * time.Millisecond)
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	fastServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	slowURL := slowServer.URL
+	fastURL := fastServer.URL
+
+	expected := fastURL
+	actual := Racer(slowURL, fastURL)
+
+	if expected != actual {
+		t.Errorf("got %q, want %q", actual, expected)
+	}
+}
