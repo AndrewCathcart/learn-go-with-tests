2f1d7726c7f8375d13d867223340ce71ae661486
Andrew Cathcart <newcastleandrewc@gmail.com>
2020-11-11 04:19:05 +0000

    numerals: refactor

--- roman_numerals/numerals_test.go
+++ roman_numerals/numerals_test.go
@@ -3,21 +3,21 @@ package main
 import "testing"
 
 func TestRomanNumerals(t *testing.T) {
-	t.Run("1 gets converted to I", func(t *testing.T) {
-		actual := ConvertToRoman(1)
-		expected := "I"
+	cases := []struct {
+		Description string
+		Arabic      int
+		Expected    string
+	}{
+		{"1 gets converted to I", 1, "I"},
+		{"2 gets converted to II", 2, "II"}
+	}
 
-		if actual != expected {
-			t.Errorf("got %q, expected %q", actual, expected)
+	for _, test := range cases {
+		t.Run(test.Description, func(t *testing.T) {
+			actual := ConvertToRoman(test.Arabic)
+			if actual != test.Expected {
+				t.Errorf("got %q, want %q", got, test.Want)
 		}
-	})
-
-	t.Run("2 gets converted to II", func(t *testing.T) {
-		actual := ConvertToRoman(2)
-		expected := "II"
-
-		if actual != expected {
-			t.Errorf("got %q, expected %q", actual, expected)
-		}
-	})
+		})
+	}
 }
