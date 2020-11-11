aeb94a081a00bc31ae910029f12dfe3b2618a765
Andrew Cathcart <newcastleandrewc@gmail.com>
2020-11-11 04:14:57 +0000

    numerals: green

---
+++ roman_numerals/numerals.go
@@ -0,0 +1,7 @@
+package main
+
+// ConvertToRoman converts arabic numbers (0-9) to roman numerals
+// E.g. III = 3, IV = 4, V = 5, etc.
+func ConvertToRoman(arabic int) string {
+	return "I"
+}
--- roman_numerals/numerals_test.go
+++ roman_numerals/numerals_test.go
@@ -2,8 +2,6 @@ package main
 
 import "testing"
 
-// Roman numerals kata - convert arabic number (0-9) to a Roman Numeral
-// E.g. III = 3, IV = 4, V = 5, etc.
 func TestRomanNumerals(t *testing.T) {
 	actual := ConvertToRoman(1)
 	expected := "I"
