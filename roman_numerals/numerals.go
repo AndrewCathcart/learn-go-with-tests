093d42f73ad9133d4dea6a5436f4418c6e82e60d
Andrew Cathcart <newcastleandrewc@gmail.com>
2020-11-11 04:24:59 +0000

    numerals: refactor

--- roman_numerals/numerals.go
+++ roman_numerals/numerals.go
@@ -5,14 +5,13 @@ import "strings"
 // ConvertToRoman converts arabic numbers (0-9) to roman numerals
 // E.g. III = 3, IV = 4, V = 5, etc.
 func ConvertToRoman(arabic int) string {
-
-	if arabic == 4 {
-		return "IV"
-	}
-
 	var result strings.Builder
 
-	for i := 0; i < arabic; i++ {
+	for i := arabic; i > 0; i-- {
+		if i == 4 {
+			result.WriteString("IV")
+			break
+		}
 		result.WriteString("I")
 	}
 
